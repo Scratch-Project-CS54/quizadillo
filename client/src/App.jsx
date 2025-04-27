aaabb5844e66ad169801029b4f491ff51e32cd03
jiaqiwang1105 <jiaqiwang1105@gmail.com>
2025-04-26 21:30:12 -0700

    game page done, pending for test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import Profile from './components/profile/profile';
+//import Profile from './components/profile/profile';
 import Game from './components/game/game';
-import Login from './components/login/login';
+//import Login from './components/login/login';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -13,16 +13,16 @@ function App() {
   return (
     <Router>
       <div>
-        <h1>Quizadillo</h1>;
-        <nav className='navigation-bar'>
-          <Link to='/login'>Login</Link>
-          <Link to='/profile'>My Profile</Link>
+        <h1>Quizadillo</h1>
+        <nav className='navigationbar'>
+          {/*  <Link to='/login'>Login</Link>  */}
+          {/* <Link to='/profile'>My Profile</Link> */}
           <Link to='/game'>Start Game</Link>
         </nav>
         <Routes>
-          <Route path='/profile' element={<Profile />} />
+          {/*<Route path='/profile' element={<Profile />} />*/}
           <Route path='/game' element={<Game />} />
-          <Route path='/login' element={<Login />} />
+          {/*<Route path='/login' element={<Login />} />*/}
         </Routes>
       </div>
     </Router>
