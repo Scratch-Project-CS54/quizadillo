536e545fc20589a49116fb16be453a4427ab4dae
Eric Shankman <ershankman@gmail.com>
2025-04-28 17:47:14 -0400

    working copy with profile working

--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './profile.module.css';
 
 export default function Profile() {
-  const [quizResult, setQuizResult] = useState(null);
+  const [quizResult, setQuizResult] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -10,12 +10,14 @@ export default function Profile() {
   useEffect(() => {
     const fetchQuizResults = async () => {
       try {
-        const response = await fetch('http://localhost:4000/api/trivia/getResults');
+        const response = await fetch('http://localhost:4000/history');
         if (!response.ok) {
           throw new Error('Failed to fetch quiz results');
         }
         const data = await response.json();
+        console.log(data);
         setQuizResult(data);
+        console.log(quizResult);
       } catch (err) {
         console.error('Error fetching quiz result:', err);
         setError(err.message);
@@ -31,33 +33,13 @@ export default function Profile() {
   if (error) return <h2>Error: {error}</h2>;
   if (!quizResult) return <h2>No quiz results found.</h2>;
 
-  const score = quizResult.score;
-  const totalQuestions = quizResult.totalQuestions;
-  const percentage = ((score / totalQuestions) * 100).toFixed(2);
+  // const score = quizResult.score;
+  // const totalQuestions = quizResult.totalQuestions;
+  // const percentage = ((score / totalQuestions) * 100).toFixed(2);
 
   return (
     <div>
       <h1 className={styles.title}>My Profile</h1>
-
-      {/* Score Summary */}
-      <table className={styles.scoreBoard}>
-        <thead>
-          <tr>
-            <th>Score</th>
-            <th>Total Questions</th>
-            <th>Percentage</th>
-            <th>Date Answered</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{score}</td>
-            <td>{totalQuestions}</td>
-            <td>{percentage}%</td>
-            <td>{new Date(quizResult.date).toLocaleDateString()}</td> {/*might be able to pull this from database */}
-          </tr>
-        </tbody>
-      </table>
 
       {/* Question Breakdown */}
       <h2 className={styles.Subtitle}>Question Breakdown</h2>
@@ -70,7 +52,7 @@ export default function Profile() {
           </tr>
         </thead>
         <tbody>
-          {quizResult.history.map((item, index) => (
+          {quizResult.map((item, index) => (
             <tr key={index}>
               <td>{item.question}</td>
               <td>{item.userAnswer}</td>
