51eec03d284171227894353d5d3d7f9321d4a32e
Eric Shankman <ershankman@gmail.com>
2025-04-28 14:13:08 -0400

    update after lunch with working copy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,15 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import Profile from './components/profile/profile';
+//import Profile from './components/profile/profile';
 import Game from './components/game/game';
+<<<<<<< HEAD
 import Login from './components/login/login';
 import Home from './components/home/home';
 import Navbar from './components/navbar/navbar';
+=======
+//import Login from './components/login/login';
+>>>>>>> aaabb58 (game page done, pending for test)
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -19,12 +23,25 @@ function App() {
   return (
     <Router>
       <div>
+<<<<<<< HEAD
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/game" element={<Game />} />
           <Route path="/login" element={<Login />} />
+=======
+        <h1>Quizadillo</h1>
+        <nav className='navigationbar'>
+          {/*  <Link to='/login'>Login</Link>  */}
+          {/* <Link to='/profile'>My Profile</Link> */}
+          <Link to='/game'>Start Game</Link>
+        </nav>
+        <Routes>
+          {/*<Route path='/profile' element={<Profile />} />*/}
+          <Route path='/game' element={<Game />} />
+          {/*<Route path='/login' element={<Login />} />*/}
+>>>>>>> aaabb58 (game page done, pending for test)
         </Routes>
       </div>
     </Router>
--- client/src/components/game/game.jsx
+++ client/src/components/game/game.jsx
@@ -14,16 +14,25 @@ export default function Game() {
 
   function getQuestions() {
     //const randomQ = Math.floor(Math.random() * 50);
+<<<<<<< HEAD
     fetch('http://localhost:5000/questions')
       .then((res) => res.json())
       .then((data) => {
         setQuestions(data); // data is an array of questions!
+=======
+    fetch('http://localhost:5000/api/trivia/questions')
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data); // data is an array of questions!
+        setLoading(false);
+>>>>>>> aaabb58 (game page done, pending for test)
         console.log(data);
       })
       .catch((err) => console.error(err));
   }
 
   function handleAnswer(answer) {
+<<<<<<< HEAD
     if (answer === questions[currentQuestionIndex].correct_answer) setScore((prev) => prev + 1);
 
     setCurrentQuestionIndex((prev) => prev + 1);
@@ -32,6 +41,23 @@ export default function Game() {
   return (
     <div>
       <QuestionCard questionObj={questions[currentQuestionIndex]} handleAnswer={handleAnswer} />
+=======
+    if (answer === questions[currentQuestionIndex].correct_answer)
+      setScore((prev) => prev + 1);
+
+    setCurrentQuestionIndex((prev) => prev + 1);
+  }
+
+  if (currentQuestionIndex >= questions.length)
+    return <h2>Game Over! Final Score: {score}</h2>;
+
+  return (
+    <div>
+      <QuestionCard
+        questionObj={questions[currentQuestionIndex]}
+        handleAnswer={handleAnswer}
+      />
+>>>>>>> aaabb58 (game page done, pending for test)
 
       <p>Score: {score}</p>
     </div>
