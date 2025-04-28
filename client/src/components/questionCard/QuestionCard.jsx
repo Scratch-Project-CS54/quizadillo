7d6b45cab90bb86c04c335c6286edbea2f585892
Eric Shankman <ershankman@gmail.com>
2025-04-28 16:20:48 -0400

    working copy login to server and some small styling

--- client/src/components/questionCard/QuestionCard.jsx
+++ client/src/components/questionCard/QuestionCard.jsx
@@ -1,16 +1,23 @@
 import styles from './questioncard.module.css';
 
-const QuestionCard = ({ questionObj, handleAnswer }) => {
+const QuestionCard = ({ questionObj, handleAnswer, handleAnswerSubmit }) => {
   if (!questionObj) return <h2>Loading...</h2>;
   const { question, correct_answer, incorrect_answers } = questionObj;
 
   const allAnswers = [...incorrect_answers, correct_answer].sort(() => Math.random() - 0.5);
 
   return (
-    <div>
-      <h2 dangerouslySetInnerHTML={{ __html: question }} />
+    <div className={styles.questionBox}>
+      <h2 className={styles.title} dangerouslySetInnerHTML={{ __html: question }} />
       {allAnswers.map((answer, index) => (
-        <button key={index} onClick={() => handleAnswer(answer)}>
+        <button
+          className={styles.btns}
+          key={index}
+          onClick={() => {
+            handleAnswer(answer);
+            handleAnswerSubmit(answer);
+          }}
+        >
           <span dangerouslySetInnerHTML={{ __html: answer }} />
         </button>
       ))}
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,5 +7,8 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:4000',
     },
+    server: {
+      host: '0.0.0.0',
+    },
   },
 });
